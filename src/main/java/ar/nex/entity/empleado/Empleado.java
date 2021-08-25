c47daa8b43ba69ae52c974358bff3b885ac0981b
Renzo - Oficina <renzog6@gmail.com>
2021-08-25 17:51:56 -0300

    update Vacaciones

--- src/main/java/ar/nex/entity/empleado/Empleado.java
+++ src/main/java/ar/nex/entity/empleado/Empleado.java
@@ -17,8 +17,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 /**
  *
