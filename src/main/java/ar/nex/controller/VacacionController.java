320ef109b7acee26d8f6d89b11b20fcdd635c2b5
Renzo - Oficina <renzog6@gmail.com>
2021-08-25 18:24:23 -0300

    update Vacaciones

--- src/main/java/ar/nex/controller/VacacionController.java
+++ src/main/java/ar/nex/controller/VacacionController.java
@@ -1,12 +1,15 @@
 package ar.nex.controller;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,12 +19,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.nex.entity.empleado.Empleado;
 import ar.nex.entity.empleado.Vacacion;
 import ar.nex.repository.EmpleadoRepository;
+import ar.nex.repository.PersonaRepository;
 import ar.nex.repository.VacacionRepository;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
@@ -33,10 +42,10 @@ import ar.nex.repository.VacacionRepository;
 public class VacacionController {
 
     @Autowired
-    EmpleadoRepository empleadoRepo;
+    private EmpleadoRepository empleadoRepo;
 
     @Autowired
-    VacacionRepository vacacionRepo;
+    private VacacionRepository vacacionRepo;
 
     @GetMapping("/{idEmpleado}")
     public ResponseEntity<List<Vacacion>> getVacacionByEmpleado(@PathVariable("idEmpleado") long id) {
@@ -61,10 +70,14 @@ public class VacacionController {
     }
 
     @PostMapping("/{idEmpleado}/create")
-    public ResponseEntity<Vacacion> createVacacion(@PathVariable("idEmpleado") long idEmpleado,
+    public ResponseEntity<Vacacion> create(@PathVariable("idEmpleado") long idEmpleado,
             @RequestBody Vacacion vacacion) {
         try {
-            Optional<Empleado> _empleado = empleadoRepo.findById(idEmpleado);
+            System.out.println("ID::::: " + idEmpleado);
+            Optional<Empleado> _empleado = empleadoRepo.findByIdPersona(idEmpleado);
+            if (_empleado.get().getVacaciones() == null) {
+                System.out.println("esnull");
+            }
 
             Vacacion newVacacion = new Vacacion();
             newVacacion.setEmpleado(_empleado.get());
@@ -82,17 +95,16 @@ public class VacacionController {
 
             Vacacion _vacacion = vacacionRepo.save(newVacacion);
 
-            System.out.println("Vacacion::: " + _vacacion.toString());
-
             return new ResponseEntity<>(_vacacion, HttpStatus.CREATED);
         } catch (Exception e) {
+            System.out.println("ID::::: " + idEmpleado);
             System.out.println(e);
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Vacacion> updateVacacion(@PathVariable("id") long id, @RequestBody Vacacion vacacion) {
+    public ResponseEntity<Vacacion> update(@PathVariable("id") long id, @RequestBody Vacacion vacacion) {
 
         Optional<Vacacion> vacacionData = vacacionRepo.findById(id);
 
@@ -105,21 +117,33 @@ public class VacacionController {
         }
     }
 
-    @GetMapping("/{idEmpleado}/dias")
-    public ResponseEntity<Integer> getDias(@PathVariable("idEmpleado") long idEmpleado) {
+    @GetMapping(value = "/diasDisponibles")
+    public ResponseEntity<?> getDias() {
         try {
-            Optional<Empleado> empleadoData = empleadoRepo.findById(idEmpleado);
-            int dias = 0;
+            List<Empleado> lista = new ArrayList<Empleado>();
+            empleadoRepo.findAll().forEach(lista::add);
 
-            if (empleadoData.isPresent()) {
-                Empleado empleado = empleadoData.get();
-                if (empleado.getVacaciones() != null) {
-                    for (Vacacion v : empleado.getVacaciones()) {
-                        dias += v.getDias();
+            JSONArray array = new JSONArray();
+            // List<JSONObject> array = new ArrayList<JSONObject>();
+            // Map<String, Object> body = new HashMap<>();
+
+            if (!lista.isEmpty()) {
+
+                for (Empleado empleado : lista) {
+
+                    int dias = 0;
+                    if (empleado.getVacaciones() != null) {
+                        for (Vacacion v : empleado.getVacaciones()) {
+                            dias += v.getDias();
+                        }
                     }
-                }
+                    JSONObject item = new JSONObject();
+                    item.put("id", empleado.getIdPersona());
+                    item.put("dias", dias);
+                    array.put(item);
 
-                return new ResponseEntity<>(dias, HttpStatus.OK);
+                }
+                return new ResponseEntity<>(array.toString(), HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
@@ -133,11 +157,12 @@ public class VacacionController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<HttpStatus> deleteVacacion(@PathVariable("id") long id) {
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") long id) {
         try {
             vacacionRepo.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e) {
+            System.out.println(e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
